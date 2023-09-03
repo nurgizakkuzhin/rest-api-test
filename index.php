2fbc65d76112853571eea88f8c1b6b46b97859c6
Nurgiz <akkuzhin.n@gmail.com>
2023-09-03 15:05:26 +0300

    json and xml

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
 
+$xml = simplexml_load_file('test.xml');
+
+foreach ($xml->product as $product) {
+    echo 'Название: ' . $product->name;
+    echo '<br>';
+    echo 'Категория: ' . $product->category;
+    echo '<br>';
+    echo 'Цена: ' . $product['cost'] . '; Количество: ' . $product['amount'];
+}
